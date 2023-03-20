74cd1504016a9c4780e851cc35685f73a153ab4c
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2023-03-20 22:51:26 +0100

    code refactoring

--- objects.py
+++ objects.py
@@ -1,29 +1,26 @@
-
 class Box:
-
     def __init__(self, width: int, height: int, depth: int, *args, **kwargs) -> None:
-        
+
         for obj in (width, height, depth):
             if type(obj) is not int:
                 raise TypeError('Parameters should be of type int and is of type', type(obj))
             if obj <= 0:
                 raise ValueError('Parameters should be greater than 0')
-            
+
         self.width = width
         self.height = height
         self.depth = depth
         self.volume = width * height * depth
-        self.coordinates = {'x': 0, 'y': 0, 'z':0}
+        self.coordinates = {'x': 0, 'y': 0, 'z': 0}
 
-    def assign_coordinates(self, x: int, y:int, z:int):
+    def assign_coordinates(self, x: int, y: int, z: int):
         self.coordinates["x"] = x
         self.coordinates["y"] = y
         self.coordinates["z"] = z
-        return self.coordinates 
-    
+        return self.coordinates
+
 
 class Container(Box):
-
     def __init__(self, width: int, height: int, depth: int, *args, **kwargs) -> None:
         super().__init__(width, height, depth)
         self.packed = []
@@ -35,16 +32,16 @@ class Container(Box):
             sum_box_volume += box.volume
         self.remaining_volume = self.volume - sum_box_volume
         return self.remaining_volume
-    
-    def add_box(self, box: Box)-> list:
+
+    def add_box(self, box: Box) -> int:
         if box.volume > self.remaining_volume:
             print("Element too large to fit into box")
         else:
             self.packed.append(box)
             self.remaining_volume = self.calculate_remaining_volume()
         return self.remaining_volume
-    
-    def check_coordinates_in_container(self, box:Box) -> bool:
+
+    def check_coordinates_in_container(self, box: Box) -> bool:
         if box.coordinates["x"] < self.coordinates["x"] or box.coordinates["x"] + box.width > self.coordinates["x"] + self.width:
             return False
         if box.coordinates["y"] < self.coordinates["y"] or box.coordinates["y"] + box.height > self.coordinates["y"] + self.height:
@@ -52,21 +49,18 @@ class Container(Box):
         if box.coordinates["z"] < self.coordinates["z"] or box.coordinates["z"] + box.depth > self.coordinates["z"] + self.depth:
             return False
         return True
-        
 
-    def check_box_collision(static_box:Box, checked_box:Box) -> bool:
-        if (checked_box.coordinates["x"] > static_box.coordinates["x"] and checked_box.coordinates["x"] < checked_box.coordinates["x"] + checked_box.width ):
+    def check_box_collision(self: Box, checked_box: Box) -> bool:
+        if self.coordinates["x"] < checked_box.coordinates["x"] < checked_box.coordinates["x"] + checked_box.width:
             return False
 
-        return True 
+        return True
 
-    
     def assign_box_position(self):
-        
+
         return
 
-        
-        
+
 a_b = b_b = c_b = 1
 a_c = b_c = c_c = 4
 
@@ -88,4 +82,3 @@ my_container.add_box(my_static_box)
 
 print("Remaining volume is ", my_container.remaining_volume)
 print("List of packed boxes ", my_container.packed)
-
