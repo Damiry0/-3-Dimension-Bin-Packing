88a89ad9ee9151a620561374cb2c53f633e04674
Grzegorz Rydzyński <rydzynskig@gmail.com>
2023-06-25 23:27:38 +0200

    Code refactoring

--- BestFit/box_collision.py
+++ BestFit/box_collision.py
@@ -1,22 +1,22 @@
-def check_rect_collision(rect_1, rect_2, dim_1, dim_2):
-    rect_1_dim = rect_1.get_packing_configuration()
-    rect_2_dim = rect_2.get_packing_configuration()
+def check_box_wall_collision(box1, box2, axis1, axis2):
+    box1_dim = box1.get_packing_configuration()
+    box2_dim = box2.get_packing_configuration()
 
-    cx1 = rect_1.position[dim_1] + rect_1_dim[dim_1] / 2
-    cy1 = rect_1.position[dim_2] + rect_1_dim[dim_2] / 2
-    cx2 = rect_2.position[dim_1] + rect_2_dim[dim_1] / 2
-    cy2 = rect_2.position[dim_2] + rect_2_dim[dim_2] / 2
+    check_axis1_box1 = box1.position[axis1] + box1_dim[axis1] / 2
+    check_axis2_box1 = box1.position[axis2] + box1_dim[axis2] / 2
+    check_axis1_box2 = box2.position[axis1] + box2_dim[axis1] / 2
+    check_axis2_box2 = box2.position[axis2] + box2_dim[axis2] / 2
 
-    ix = max(cx1, cx2) - min(cx1, cx2)
-    iy = max(cy1, cy2) - min(cy1, cy2)
+    intersect_x_axis = max(check_axis1_box1, check_axis1_box2) - min(check_axis1_box1, check_axis1_box2)
+    intersect_y_axis = max(check_axis2_box1, check_axis2_box2) - min(check_axis2_box1, check_axis2_box2)
 
-    return ix < (rect_1_dim[dim_1] + rect_2_dim[dim_1]) / 2 and iy < (rect_1_dim[dim_2] + rect_2_dim[dim_2]) / 2
+    return intersect_x_axis < (box1_dim[axis1] + box2_dim[axis1]) / 2 and intersect_y_axis < (box1_dim[axis2] + box2_dim[axis2]) / 2
 
 
 def box_collision(box1, box2):
-    return (check_rect_collision(box1, box2, 0, 1) and
-            check_rect_collision(box1, box2, 1, 2) and
-            check_rect_collision(box1, box2, 0, 2))
+    return (check_box_wall_collision(box1, box2, 0, 1) and
+            check_box_wall_collision(box1, box2, 1, 2) and
+            check_box_wall_collision(box1, box2, 0, 2))
 
 
 
